36bf87dbb299788cd7ce679abc448011556dd596
Nico Hermida <hermida.nicolas101@gmail.com>
2025-05-12 00:31:39 -0300

    add models, serializers, urls and views for admin, empleado and surcursales

--- api/alquilapp/urls.py
+++ api/alquilapp/urls.py
@@ -5,6 +5,9 @@ from alquilapp import views
 router = routers.DefaultRouter()
 router.register(r'vehiculos', views.VehiculoView, basename='vehiculo')
 router.register(r'cancelaciones', views.CancelacionView, basename='cancelacion')
+router.register(r'admins', views.AdminViewSet, basename='admin')
+router.register(r'empleados', views.EmpleadoViewSet, basename='empleado')
+router.register(r'sucursales', views.SucursalViewSet, basename='sucursal')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
