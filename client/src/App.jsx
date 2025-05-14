df9aa0042323abe2a3bfc23270e8e8f0ba337b63
Nico Hermida <hermida.nicolas101@gmail.com>
2025-05-14 17:57:15 -0300

    fix homepage width

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
