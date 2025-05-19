e3bbff3b4b90868b9a46a78f9a38da65ed5ffaca
ivan farid <ivan99edlp@gmail.com>
2025-05-19 20:18:52 -0300

    minor fixes on db models

--- api/alquilapp/models.py
+++ api/alquilapp/models.py
@@ -129,8 +129,7 @@ class CategoriaVehiculo(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=32)
     precio = models.DecimalField(max_digits=7, decimal_places=2)
-    carlos = models.CharField(max_length=20, null=True, blank=True)
-    roberto = models.CharField(max_length=20, null=True, blank=True)
+
     def __str__(self):
         return f"{self.nombre}"
 # TODO: Ver modelo Alquiler que requiere algo de aca. En base al precio de la categoria, calcula el precio del alquiler.
