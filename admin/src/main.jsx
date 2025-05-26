f6cec6ba4946917871e9adc4c530244ec44bb32e
Nico Hermida <hermida.nicolas101@gmail.com>
2025-05-26 11:08:05 -0300

    add authentication in admin site

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,22 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { HeroUIProvider } from "@heroui/react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { DashboardPage } from "./pages/DashboardPage.jsx";
-import { LoginPage } from "./pages/LoginPage.jsx";
-import VehiculoPage from "./pages/VehiculoPage.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { HeroUIProvider } from '@heroui/react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { DashboardPage } from './pages/DashboardPage.jsx'
+import { LoginPage } from './pages/LoginPage.jsx'
+import VehiculoPage from './pages/VehiculoPage.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <HeroUIProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<DashboardPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/vehiculo" element={<VehiculoPage />} />
-                </Routes>
-            </BrowserRouter>
-        </HeroUIProvider>
-    </StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<HeroUIProvider>
+			<AuthProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/' element={<DashboardPage />} />
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/vehiculo' element={<VehiculoPage />} />
+					</Routes>
+				</BrowserRouter>
+			</AuthProvider>
+		</HeroUIProvider>
+	</StrictMode>
+)
--- admin/src/pages/DashboardPage.jsx
+++ admin/src/pages/DashboardPage.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from 'react-router'
 import { DashboardLayout } from '../components/DashboardLayout'
 import { RegisterEmployeeForm } from '../components/RegisterEmployeeForm'
+import { useAuth } from '../contexts/AuthContext'
+import { useEffect } from 'react'
 
 export function DashboardPage() {
+	const { isAuthenticated } = useAuth()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!isAuthenticated) {
+			navigate('/login')
+		}
+	}, [])
+
+	if (!isAuthenticated) return ''
+
 	return (
 		<DashboardLayout>
 			<section className='w-full py-[135px] flex items-center justify-center bg-gray-100'>
