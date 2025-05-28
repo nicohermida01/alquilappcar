848e923cf4498ef225a94aa129d70fd7cbee3593
Nico Hermida <hermida.nicolas101@gmail.com>
2025-05-28 13:19:15 -0300

    fix import in admin site

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,4 +1,3 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,23 +5,22 @@ import { HeroUIProvider, ToastProvider } from '@heroui/react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { DashboardPage } from './pages/DashboardPage.jsx'
 import { LoginPage } from './pages/LoginPage.jsx'
-import VehiculoPage from './pages/VehiculoPage.jsx'
+import VehiclesPage from './pages/VehiclesPage.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<HeroUIProvider>
 			<AuthProvider>
-        <ToastProvider />
+				<ToastProvider />
 				<BrowserRouter>
 					<Routes>
 						<Route path='/' element={<DashboardPage />} />
 						<Route path='/login' element={<LoginPage />} />
-						<Route path='/vehiculo' element={<VehiculoPage />} />
+						<Route path='/vehiculo' element={<VehiclesPage />} />
 					</Routes>
 				</BrowserRouter>
 			</AuthProvider>
 		</HeroUIProvider>
 	</StrictMode>
 )
-
