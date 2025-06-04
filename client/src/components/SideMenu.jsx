e1334c1d369677b8c7809fd992a627536dec93ae
ivan farid <ivan99edlp@gmail.com>
2025-06-04 17:03:21 -0300

    added modal on cancelar alquiler

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,6 +1,6 @@
 function SideMenu({ onSelect }) {
 	return (
-		<div className="w-64 bg-color-2 text-white h-screen">
+		<div className="w-64 bg-color-2 text-white h-100">
 			<ul className="sticky p-4 space-y-2 top-0">
 				<li className="bg-color-5 hover:bg-color-4 p-2 rounded cursor-pointer" onClick={() => onSelect('myinfo')}>
 					<p>Mis datos</p>
